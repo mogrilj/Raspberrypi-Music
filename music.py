9866f73c7d46f9fe2458fed506d8fa92368eb00b
Mo Grilj <43280378+mogrilj@users.noreply.github.com>
2019-03-15 18:09:53 +0100

    Update music.py

--- music.py
+++ music.py
@@ -4,7 +4,7 @@ import urllib.request
 def run(name):
     name = name.replace(" ","%20")
     
-    urllib.request.urlretrieve("http://mo.flussbuero.at/music/hochgeladenes/"+name+".mp3",name+".mp3")
+    urllib.request.urlretrieve("url_to_music_folder"+name+".mp3",name+".mp3")
     os.system("start "+name+".mp3")
     os.remove(name+".mp3")
                                
